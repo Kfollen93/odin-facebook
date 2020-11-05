6838fc35288ca313293c83c1ff4eebd1d8f4fa56
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-11-05 08:46:24 -0800

    Install Devise gem and create User model and controller

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # I will need to set mailer to production URL once ready **
 end
