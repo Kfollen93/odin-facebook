7b35f66d941fbdfd8b4d31059d2911613890b5bb
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-11-07 22:25:28 -0800

    Add helper methods to use devise sign in form on main page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  # Helper methods to render Devise's log in view on welcome page
+  
+  helper_method :resource, :resource_name, :devise_mapping
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
 end
