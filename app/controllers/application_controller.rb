8a88e29d85fe72101ed16bd00b3e4d6212505427
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-11-10 16:55:47 -0800

    Add name field to devise registration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
+  end
+
   # Helper methods to render Devise's log in view on welcome page
-  
   helper_method :resource, :resource_name, :devise_mapping
 
   def resource_name
--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2020_11_10_194539) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "username" # Decided not to use, will remove.
     t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
