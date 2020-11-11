98fcc5f55bbb45f844731185887ac38e52c24273
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-11-10 16:58:19 -0800

    Remove username column from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_194539) do
+ActiveRecord::Schema.define(version: 2020_11_11_005743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 2020_11_10_194539) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username" # Decided not to use, will remove.
     t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -45,7 +44,6 @@ ActiveRecord::Schema.define(version: 2020_11_10_194539) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
