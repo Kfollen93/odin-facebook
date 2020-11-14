bb638ab400abebafc729c4667ce5128431a4f718
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-11-14 14:59:03 -0800

    Add initial user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,23 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'invalid without name' do
+    user = User.new(name: 'John')
+    refute user.valid?, 'user is valid without a name'
+    assert_not_nil user.errors[:name], 'no validation error for name present'
+  end
+
+  test 'invalid without email' do
+    user = User.new(email: 'john@example.com')
+    refute user.valid?, 'user is valid without a name'
+    assert_not_nil user.errors[:email], 'no validation error for email present'
+  end
+
+  test 'invalid without password' do
+    user = User.new(password: 'testing')
+    refute user.valid?, 'user is valid without a password'
+    assert_not_nil user.errors[:password], 'no validation error for password present'
+  end
+
 end
