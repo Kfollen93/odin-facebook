10703af5c773ddeb56c3ed8cbe2f75226f962516
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-11-18 13:52:15 -0800

    Add delete image method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def delete_image
+    image = ActiveStorage::Attachment.find(params[:image_id])
+    if current_user == image.record
+      image.purge
+      redirect_back(fallback_location: request.referer)
+    else
+      redirect_to root_url, notice: "You can't do that!"
+    end
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :confirmation_password)
+    params.require(:user).permit(:name, :email, :password, :confirmation_password, :profile_picture)
   end
 
 end
