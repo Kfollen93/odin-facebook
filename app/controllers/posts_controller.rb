a211843916783a50895a5b9efc2eba9d64938a51
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-11-29 15:59:54 -0800

    Add post methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,27 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all #this will need to become current_user's posts and their friends only.
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect to @post
+    else
+      render 'new'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
