dfe82449ddd49b47738599ae123bafc69aa02dbe
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-12-08 17:35:22 -0800

    Nest routes for posts and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 devise_for :users
-resources :posts, :comments
+resources :posts do
+   resources :comments
+end
 
 resources :users, only: [:index, :show, :edit, :update] do 
   member do
